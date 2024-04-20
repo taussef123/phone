b13b584c418dc93102de7a7b91f25d4058fa0eb9
MdTauseefAhmed <metausifahmed30899@gmail.com>
2024-04-20 23:41:57 +0530

    first commit

---
+++ vicky.js
@@ -0,0 +1,9 @@
+console.log("script is running....")
+
+
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+
+})
+
